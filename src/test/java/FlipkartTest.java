77b8be4e6c4bae8c0f009bc07cb2feb27a46949d
ADITYA DUBEY <adidubs9026@gmail.com>
2025-12-31 18:13:27 +0530

    updated the Test file

--- src/test/java/FlipkartTest.java
+++ src/test/java/FlipkartTest.java
@@ -11,20 +11,21 @@ public class FlipkartTest {
     @Test
     public void openFlipkart() {
         logger.info("Starting Flipkart Selenium test");
-
+        System.out.println("Starting Flipkart Selenium test");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
         logger.debug("Chrome options configured");
+        System.out.println("Chrome options configured");
 
         WebDriver driver = new ChromeDriver(options);
         logger.info("ChromeDriver initialized");
-
+        System.out.println("ChromeDriver initialized");
         driver.get("https://www.flipkart.com");
         logger.info("Navigated to Flipkart website");
-
+        System.out.println("Navigated to Flipkart website");
         String title = driver.getTitle();
         logger.info("Page title: {}", title);
         System.out.println("Page title: " + title);
